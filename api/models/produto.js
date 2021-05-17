b59f53c0347c0bab97264a458097fa3e5e3c89a0
Rene X <renefx@gmail.com>
2021-05-16 23:43:55 -0300

    validacao com Joi

--- api/models/produto.js
+++ api/models/produto.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Joi = require("../util/joi");
 
 var produtoSchema = new mongoose.Schema({
   nome: {
@@ -20,6 +21,24 @@ var produtoSchema = new mongoose.Schema({
     default: false,
   },
 });
+
+const joiSchema = Joi.object({
+  nome: Joi.string().min(1, "utf8").trim().required(),
+  preco: Joi.number().positive().precision(2).required(),
+  descricao: Joi.string().min(10, "utf8"),
+  imagem: Joi.string(),
+  permiteAlteracao: Joi.boolean(),
+}).options({
+  abortEarly: false,
+});
+
+produtoSchema.methods.joiValidate = function (obj) {
+  // valida em inglês
+  // let { error } = joiSchema.validate(obj);
+  // valida em portugues
+  let errorPtBr = Joi.validatePt(joiSchema, obj);
+  return errorPtBr;
+};
 
 //Export the model
 module.exports = mongoose.model("Produto", produtoSchema);
